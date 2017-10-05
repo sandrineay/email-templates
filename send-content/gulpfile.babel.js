af9458e37ab175591aca34531e2c4a75a26843ce
cedricmenteau <me.cedric@gmail.com>
2017-10-05 15:10:29 +0200

    Final touch

--- send-content/gulpfile.babel.js
+++ send-content/gulpfile.babel.js
@@ -129,7 +129,7 @@ function inliner(css) {
     .pipe($.replace, '<link rel="stylesheet" type="text/css" href="css/app.css">', '')
     .pipe($.htmlmin, {
       collapseWhitespace: false,
-      minifyCSS: false
+      minifyCSS: true
     });
 
   return pipe();
